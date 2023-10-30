bc6ec88a32b9bb621bffe373fe91d5b575c1e1de
ruanm <247987314@qq.com>
2023-10-31 02:14:09 +0800

    feat:nprogress安装与使用详解，vue路由加载进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import type { App } from 'vue'
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+// 引入 nprogress 相关方法
+import { close, start } from '~/utils/nprogress'
 // 引入 login.ts
 import LoginRouter from './modules/login'
 // 引入 test.ts
@@ -15,9 +17,21 @@ export const publicRoutes: Array<RouteRecordRaw> = [
 	}
 ]
 
+// 定义router
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes: publicRoutes
+})
+
+// 路由前置后卫
+router.beforeEach(() => {
+	// 开启进度条
+	start()
+})
+// 路由后置后卫
+router.afterEach(() => {
+	// 关闭进度条
+	close()
 })
 
 /* 初始化路由表 */
